29c775530bb287e0fb9996c54f5fae183974c9ea
kabir Malviya <kabirmalviya@kabirs-MacBook-Air.local>
2024-06-27 07:58:03 +0530

    logoug

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
                 <DropdownMenuItem
                   onClick={() => {
                     fn().then(() => {
-                      navigate("/auth");
+                      window.location.reload();
                     });
                   }}
                   className="text text-red-500"
