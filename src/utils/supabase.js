1cbd7407afddbb3a9f78923798a6a0fa6b360110
Kabir Malviya <101920355+K4Kabir@users.noreply.github.com>
2025-11-02 11:12:17 +0530

    Update Supabase URL and key in supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ceerhlurthkovoqmvlay.supabase.co";
+const supabaseUrl = "https://gwibzytoscsywzulyinv.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlZXJobHVydGhrb3ZvcW12bGF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyMTE4NTgsImV4cCI6MjAzNDc4Nzg1OH0.XeZXkz_mXo1NNmyFzwrtwotNZk9M6geHMRJmdXxk2h4";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aWJ6eXRvc2NzeXd6dWx5aW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5OTAyNjgsImV4cCI6MjA3NzU2NjI2OH0.vzK08OAEaanbn6X1W5ViFzs10PNcc10Cy5JsVzN7W7g";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
